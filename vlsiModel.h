2ddb64794efc7a6ef9081a0fe6bbdb1ae1a1633a
Siddardha Reddy Baddigam <siddardhareddybaddigam@gmail.com>
2024-04-15 11:53:44 +0530

    keras2c converted

---
+++ vlsiModel.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void vlsiModel(k2c_tensor* dense_input_input, k2c_tensor* dense_3_output); 
+void vlsiModel_initialize(); 
+void vlsiModel_terminate(); 
